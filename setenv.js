b7c1abc515e78a8b58f61283a94fa0711104098d
Guillermo Labra <matiaslabraantilef@Matiass-MacBook-Pro.local>
2020-01-11 16:28:34 +1000

    housekeeping files

--- setenv.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV;
-if (env === 'production') {
-  console.log(`MONGO_URL=${process.env.MONGO_URL}`);
-} else {
-  console.log(`MONGO_URL=mongodb://localhost:27017/housekeeping
-  `);
-}
