1a43be5e18675df4197a5633ee722bb5abbc8d15
Matias Labra <gmlabra@gmail.com>
2020-04-26 22:43:39 +1000

    chore: cleaning

--- app/containers/ReportPage/constants.js
+++
@@ -1,7 +0,0 @@
-/*
- *
- * ReportPage constants
- *
- */
-
-export const DEFAULT_ACTION = 'app/ReportPage/DEFAULT_ACTION';
--- app/containers/ReportPage/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectReportPageDomain } from '../selectors';
-
-describe('selectReportPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
